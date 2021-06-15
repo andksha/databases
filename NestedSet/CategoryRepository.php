c98eb71f9a63ebc3e0e746d4c6bdff89860e6225
Andrij Kshanovskij <andrewafk2000@gmail.com>
2021-06-15 19:31:00 +0300

    Add nested set implementation

---
+++ NestedSet/CategoryRepository.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+final class CategoryRepository
+{
+    public function getMany(): array
+    {
+        $category = CategoryNestedSet::query()->orderBy('lft')->get();
+
+        return $this->mapCategories($category);
+    }
+
+    public function lastLvl(): array
+    {
+        return CategoryNestedSet::query()->whereRaw('"rgt" - "lft" = 1')->get()->toArray();
+    }
+
+    public function getOne(int $id): array
+    {
+        $category = CategoryNestedSet::query()->where('id', $id)
+            ->with(['attributes'])
+            ->first();
+
+        $categories = CategoryNestedSet::query()->where('lft', '<', $category->lft)
+            ->where('rgt', '>', $category->rgt)
+            ->get()
+            ->merge([$category])
+            ->sortBy('lft');
+
+        return $this->mapCategories($categories);
+    }
+
+    private function mapCategories(Collection $categories): array
+    {
+        $tree = new CategoryTree();
+        $minDepth = $categories->min('depth');
+
+        /** @var CategoryNestedSet $value */
+        foreach ($categories as $key => $value) {
+            $node = new CategoryNode($value);
+            $tree->setLastNodeOfLvl($node, $value->depth);
+
+            if ($key === 0 || $value->depth === $minDepth) {
+                $tree->addRoot($node);
+                continue;
+            }
+
+            $tree->getLastNodeOfLvl($value->depth - 1)->addChild($node);
+        }
+
+        return $tree->toArray();
+    }
+
+    public function add(?int $parentID, string $name): CategoryNestedSet
+    {
+        if (!$parentID) {
+            return $this->addRoot($name);
+        }
+
+        return $this->addNode($parentID, $name);
+    }
+
+    private function addRoot(string $name): CategoryNestedSet
+    {
+        if (!$max = CategoryNestedSet::query()->max('rgt')) {
+            return CategoryNestedSet::query()->create([
+                "parent_id" => null,
+                "depth" => 1,
+                "name" => $name,
+                "slug" => Str::slug($name),
+                "lft" => 1,
+                "rgt" => 1,
+            ]);
+        }
+
+        return CategoryNestedSet::query()->create([
+            "parent_id" => null,
+            "depth" => 1,
+            "name" => $name,
+            "slug" => Str::slug($name),
+            "lft" => $max + 1,
+            "rgt" => $max + 2,
+        ]);
+    }
+
+    private function addNode(int $parentID, string $name): CategoryNestedSet
+    {
+        $parent = CategoryNestedSet::query()->where('id', $parentID)->first();
+        CategoryNestedSet::query()->where('lft', '>=', $parent->rgt)->update(['lft' => DB::raw('"lft" + 2')]);
+        CategoryNestedSet::query()->where('rgt', '>=', $parent->rgt)->update(['rgt' => DB::raw('"rgt" + 2')]);
+
+        return CategoryNestedSet::query()->create([
+            "parent_id" => $parentID,
+            "depth" => $parent->depth + 1,
+            "name" => $name,
+            "slug" => Str::slug($name),
+            "lft" => $parent->rgt,
+            "rgt" => $parent->rgt + 1,
+        ]);
+    }
+
+    public function delete(int $id): bool
+    {
+        $category = CategoryNestedSet::query()->where('id', $id)->first();
+
+        // assign $category's parent id to it's children, decrement lft and rgt by one, decrement depth
+        CategoryNestedSet::query()->where('lft', '>', $category->lft)
+            ->where('rgt', '<', $category->rgt)
+            ->update([
+                'parent_id' => $category->parent_id,
+                'lft' => DB::raw('"lft" - 1'),
+                'rgt' => DB::raw('"rgt" - 1'),
+                'depth' => DB::raw('"depth" - 1'),
+            ]);
+
+        // decrement rgt by 2 where rgt > $category->rgt
+        CategoryNestedSet::query()->where('rgt', '>', $category->rgt)->update([
+            'rgt' => DB::raw('"rgt" - 2'),
+        ]);
+        // decrement lft by 2 where lft > $category->rgt
+        CategoryNestedSet::query()->where('lft', '>', $category->rgt)->update([
+            'lft' => DB::raw('"lft" - 2'),
+        ]);
+
+        return $category->delete();
+    }
+
+    public function move(int $id, ?int $parentId): bool
+    {
+        $category = CategoryNestedSet::query()->where('id', $id)->first();
+        $max = CategoryNestedSet::query()->max('rgt');
+
+        if (!$parentId) {
+            $category->update([
+                'depth' => 1,
+                'parent_id' => null,
+                'lft' => $max + 1,
+                'rgt' => $max + 2
+            ]);
+        }
+
+        if (!$parent = CategoryNestedSet::query()->where('id', $parentId)->first()) {
+            return false;
+        }
+
+        if ($parent->rgt > $category->rgt) {
+            CategoryNestedSet::query()->where('lft', '>', $category->lft)
+                ->where('lft', '<', $parent->rgt)
+                ->update([
+                    'lft' => DB::raw('"lft" - 2'),
+                ]);
+            CategoryNestedSet::query()->where('rgt', '>', $category->lft)
+                ->where('rgt', '<', $parent->rgt)
+                ->update([
+                    'rgt' => DB::raw('"rgt" - 2'),
+                ]);
+
+            $category->update([
+                'depth' => $parent->depth + 1,
+                'parent_id' => $parent->id,
+                'lft' => $parent->rgt - 2,
+                'rgt' => $parent->rgt - 1,
+            ]);
+        } elseif ($parent->rgt < $category->rgt) {
+            CategoryNestedSet::query()->where('lft', '>', $parent->rgt)
+                ->where('lft', '<', $category->rgt)
+                ->update([
+                    'lft' => DB::raw('"lft" + 2'),
+                ]);
+            CategoryNestedSet::query()->where('rgt', '>=', $parent->rgt)
+                ->where('rgt', '<', $category->rgt)
+                ->update([
+                    'rgt' => DB::raw('"rgt" + 2'),
+                ]);
+
+            $category->update([
+                'depth' => $parent->depth + 1,
+                'parent_id' => $parent->id,
+                'lft' => $parent->rgt,
+                'rgt' => $parent->rgt + 1,
+            ]);
+        }
+
+        return true;
+    }
+}
